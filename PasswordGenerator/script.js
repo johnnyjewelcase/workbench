bbe017eac580c5da1fd677f84c523debaac3f512
johnnyjewelcase <61571390+johnnyjewelcase@users.noreply.github.com>
2020-03-17 16:13:13 -0700

    password generator populating possible character array with a loop

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -4,35 +4,19 @@ function generatePassword() {
   /****
    * WRITE YOUR CODE HERE
    */
-  var password = "";
-
   var pwLength = prompt("Create a password. How many characters long?");
 
   if (isNaN(pwLength)) {
-    alert("Please enter a number!");
-    return;
+    return ("Please enter a number!");
   } else if ((parseFloat(pwLength) - parseInt(pwLength)) !== 0) {
-    console.log(pwLength);
-    console.log(parseFloat(pwLength));
-    console.log(parseInt(pwLength));
-    alert("Please enter a whole number!");
-    return;
+    return ("Please enter a whole number!");
   } else if (pwLength < 8) {
-    alert("Passwords must have at least eight characters!");
-    return;
+    return ("Passwords must have at least eight characters!");
   } else if (pwLength > 128) {
-    alert("Passwords cannot be longer than 128 characters!");
-    return;
+    return ("Passwords cannot be longer than 128 characters!");
   } else {
     pwLength = parseInt(pwLength);
   }
-
-
-  var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var charSpecial = ["!", "\\", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-  var chars = [];
 
   var useLower = confirm("Include lowercase letters?");
   var useUpper = confirm("Include uppercase letters?");
@@ -44,65 +28,60 @@ function generatePassword() {
     return;
   }
 
+  // POPULATE ARRAY FROM ASCII REFERENCE: https://www.w3schools.com/charsets/ref_html_ascii.asp
+  var chars = [];
   if (useLower === true) {
-    console.log(charLower);
-    chars = chars.concat(charLower);
+    console.log("lowercase letters selected")
+    // chars = chars.concat(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+    for (var l = 97; l <= 122; l++) {
+      chars = chars.concat(String.fromCharCode(l));
+    }
   }
-
   if (useUpper === true) {
-    console.log(charUpper);
-    chars = chars.concat(charUpper);
+    console.log("uppercase letters selected")
+    // chars = chars.concat(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]);
+    for (var u = 65; u <= 90; u++) {
+      chars = chars.concat(String.fromCharCode(u));
+    }
   }
-
   if (useNumber === true) {
-    console.log(charNumber);
-    chars = chars.concat(charNumber);
+    console.log("numbers selected")
+    // chars = chars.concat(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+    for (var n = 48; n <= 57; n++) {
+      chars = chars.concat(String.fromCharCode(n));
+    }
   }
-
   if (useSpecial === true) {
-    console.log(charSpecial);
-    chars = chars.concat(charSpecial);
+    console.log("special characters selected")
+    // chars = chars.concat(["!", "\\", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]);
+    for (var s1 = 33; s1 <= 47; s1++) {
+      chars = chars.concat(String.fromCharCode(s1));
+    }
+    for (var s2 = 123; s2 <= 126; s2++) {
+      chars = chars.concat(String.fromCharCode(s2));
+    }
   }
 
-  // console.log("character array:");
-  // console.log(chars);
-  // console.log("selected pw length:");
-  // console.log(pwLength);
-
-
-  var curPwLength = 0;
-  var lastPwLength = 0;
+  var password = "";
+  console.log("possible character array:");
+  console.log(chars);
+  console.log("selected pw length: " + pwLength);
 
   for (var i = 1; i < (pwLength + 1); i++) {
-    // console.log("i:");
-    // console.log(i);
     var randomNumInRange = Math.floor(Math.random() * (chars.length));
-    // console.log("random number:");
-    // console.log(randomNumInRange);
-    // console.log("randomly selected character:");
-    // console.log(chars[randomNumInRange]);
     password = password + chars[randomNumInRange];
-    // console.log("partial password:");
-    // console.log(password);
 
-    curPwLength = password.length;
-    if (curPwLength - lastPwLength > 1) {
-      console.log("TOO MANY CHARACTERS GENERATED")
+    if (chars[randomNumInRange].length > 1) {
+      console.log("INVALID GENERATED IN THIS LOOP: " + chars[randomNumInRange]);
       break;
     }
-    lastPwLength = password.length;
   }
 
-  console.log("generated password length:");
-  console.log(password.length);
-
   if (pwLength > password.length) {
-    alert("Oops. We didn't make a long enough password. Try Again.");
-    return;
+    return ("Oops. We didn't make a long enough password. Try Again.");
   }
 
   console.log(password);
-  // alert(password);
   return (password);
 }
 
