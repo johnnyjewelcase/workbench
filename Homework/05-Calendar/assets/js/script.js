c9def1c110a586534ea67a565529d924950fae8a
johnnyjewelcase <61571390+johnnyjewelcase@users.noreply.github.com>
2020-04-02 18:52:39 -0700

    hw5 layout

--- Homework/05-Calendar/assets/js/script.js
+++ Homework/05-Calendar/assets/js/script.js
@@ -1,9 +1,62 @@
 $(document).ready(function () {
-    var date = new Date();
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var monthIndex = date.getMonth();
-    var dayIndex = date.getDay();
-    // $("#currentDay").html(days[dayIndex] + ", " + months[monthIndex] + " " + date.getDate());
-    $("#currentDay").html(moment().format('dddd, MMMM Do'));
+    $("#currentDay").html(moment().format("dddd, MMMM Do"));
+    var hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+    var currentHour = moment().format("k");
+    for (i = 0; i < hours.length; i++) {
+        var tr = $("<tr class='row'>");
+        var tdHour = $("<td class='col-2 hour'>");
+        var tdEvent = $("<td class='col-9 event'>");
+
+        var tdSave = $("<td class='col-1 saveBtn'>")
+            .css({
+                "text-align": "center",
+                padding: "25px"
+            })
+            .html("<h3><i class='fas fa-save'></i></h3>");
+
+        if (hours[i] <= 12) {
+            tdHour.html(hours[i] + " AM");
+        } else tdHour.html(hours[i] - 12 + " PM");
+
+        if (hours[i] < currentHour) {
+            tdEvent.addClass("past");
+        } else if (hours[i] == currentHour) {
+            tdEvent.addClass("present");
+        } else if (hours[i] > currentHour) {
+            tdEvent.addClass("future");
+        }
+
+        tr.append(tdHour, tdEvent, tdSave);
+
+        $("tbody").append(tr);
+    }
+
+    $(".event").click(function () {
+        if ($(this).hasClass("past")) {
+            var txt = $(this).text();
+            $(this)
+                .html("You can't create events in the past!")
+                .css("color", "red")
+                .addClass("error");
+
+            setTimeout(function () {
+                txt =
+                    $(".error").html(txt).css("color", "white");
+            }, 1000);
+        } else {
+            $("textarea").remove();
+            $(this).append($("<textarea />"));
+            $("textarea").css("width", "100%");
+            $("textarea").focus();
+        }
+    });
+
+    $(".saveBtn").click(function () {
+        var eventText = $("textarea").val();
+        console.log(eventText);
+        $(this)
+            .prev()
+            .html(eventText);
+    });
 });
