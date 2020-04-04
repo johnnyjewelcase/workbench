e292fd590598d90aa061727312c8794065a0f2c5
johnnyjewelcase <61571390+johnnyjewelcase@users.noreply.github.com>
2020-04-04 16:59:16 -0700

    hw5 99%

--- Homework/05-Calendar/README.md
+++ Homework/05-Calendar/README.md
@@ -1,1 +1,71 @@
 # Calendar
+
+When the user opens the day planner, we use moment.js to find out what the current day is and format it the way we want before putting it at the top of the page. We also note the current hour for later use.
+
+Next we initialize a few global variables to make life easier, including an array of hours in the work day.
+
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+Create a global array to store objects representing calendar events.
+
+I need a couple global variables for event handlers to share.
+
+Populate the event array from localstorage, if there's anything in localstorage.
+
+Loop through all the hours in the workday.
+Create a row for the hour currently selected by the loop.
+Create a cell to display the time that this row represents.
+Create a cell to store the event.
+Create a cell for the save icon.
+Format the cell so that the icon will be centered.
+Add the save icon from Font Awesome to the cell.
+Set an attribute for the hour of the current row for use when an event is saved.
+
+    Display the time appropriately.
+
+    Format the color of the row according to its time relative to the current time.
+
+    Loop through the events array, looking for a saved event with
+    a time property that matches the currently selected by the loop.
+
+    Add the row to the table body.
+
+Listen for clicks on the event area.
+If the user had previously clicked on another event and did not save, put the original text back.
+Store the current text and location in case we need to revert (see above).
+
+    Prevent user from attempting to alter the past.
+      Remove any textareas on the page that have already been created.
+      Hide any text in the current event.
+      Create a new textarea input in the current event and focus the cursor inside it.
+
+Listen for clicks on the save button.
+Grab the text that the user just intered and make a note of which time/row that we're on.
+
+    Get rid of the text input and replace it with the user-supplied text.
+
+    Check whether we already have any events saved.
+      If so, check for an existing event in this time period.
+          If so, delete it from the array.
+
+    Create an object to hold the newly-created event.
+
+    If we already have any events in the array, push the new one into the array.
+    If not, initialize the array with this new object.
+
+    Store the events array in localstorage for safekeeping.
+    localStorage.setItem("calendar", JSON.stringify(events));
+
+    Store the event description in a global variable to make sure that it overwrites any previous event at this time.
+
+Make the clear button delete local storage AND the events on the screen.
--- Homework/05-Calendar/assets/js/script.js
+++ Homework/05-Calendar/assets/js/script.js
@@ -1,28 +1,48 @@
 $(document).ready(function () {
   // Use moment.js to find the current day and format it to make it readable.
   $("#currentDay").html(moment().format("dddd, MMMM Do"));
-  // Create an array of hours in the workday.
-  var hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
   // Find out what time it is so that rows can be colored as specified.
   var currentHour = moment().format("k");
 
-  // Create an array to store objects representing calendar events.
-  // Seed events array with an unused row to make life a little easier.
-  var events = [
-    {
-      time: 500,
-      event: "",
-    },
+  // Create an array of hours in the workday.
+  var hours = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
   ];
 
-  // I need a global variable for event handlers to share.
+  // Create a global array to store objects representing calendar events.
+  var events;
+
+  // I need a couple global variables for event handlers to share.
   var txt;
+  var cell;
 
   // Populate the event array from localstorage, if there's anything in localstorage.
   if (localStorage.getItem("calendar") !== null) {
     events = JSON.parse(localStorage.getItem("calendar"));
   }
-  console.log(events);
 
   // Loop through all the hours in the workday.
   for (i = 0; i < hours.length; i++) {
@@ -64,19 +84,30 @@ $(document).ready(function () {
 
     // Loop through the events array, looking for a saved event with
     // a time property that matches the currently selected by the loop.
-    for (j = 0; j < events.length; j++) {
-      if (events[j].time === hours[i]) {
-        tdEvent.text(events[j].event);
+    if (events != undefined) {
+      for (j = 0; j < events.length; j++) {
+        if (events[j].time === hours[i]) {
+          tdEvent.text(events[j].event);
+        }
       }
     }
     tr.append(tdHour, tdEvent, tdSave);
 
+    // Add the row to the table body.
     $("tbody").append(tr);
   }
 
   // Listen for clicks on the event area.
   $(".event").click(function () {
+    // If the user had previously clicked on another event and did not save, put the original text back.
+    if (cell != undefined) {
+      cell.text(txt);
+    }
+    // Store the current text and location in case we need to revert (see above).
     txt = $(this).text();
+    cell = $(this);
+
+    // Prevent user from attempting to alter the past.
     if ($(this).hasClass("past")) {
       $(this)
         .text("You can't create events in the past!")
@@ -87,42 +118,67 @@ $(document).ready(function () {
         txt = $(".error").text(txt).css("color", "white");
       }, 1000);
     } else {
+      // Remove any textareas on the page that have already been created.
       $("textarea").remove();
+      // Hide any text in the current event.
       $(this).text("");
+      // Create a new textarea input in the current event and focus the cursor inside it.
       $(this).append($("<textarea />").val(txt));
       $("textarea").css("width", "100%");
       $("textarea").focus();
     }
   });
 
-  // $(".event").on("focusout", function () {
-  //   $(this).text(txt);
-  // });
-
   // Listen for clicks on the save button.
   $(".saveBtn").click(function () {
+    // Grab the text that the user just intered and make a note of which time/row that we're on.
     var eventText = $("textarea").val();
     var eventHour = parseInt($(this).attr("hour"));
 
+    // Get rid of the text input and replace it with the user-supplied text.
+    $("textarea").remove();
     $(this).prev().text(eventText);
 
-    for (j = 0; j < events.length; j++) {
-      if (events[j].time === eventHour) {
-        events.splice(j, 1);
+    // Check whether we already have any events saved.
+    if (events != undefined) {
+      // If so, check for an existing event in this time period.
+      for (j = 0; j < events.length; j++) {
+        if (events[j].time === eventHour) {
+          // If so, delete it from the array.
+          events.splice(j, 1);
+        }
       }
     }
 
+    // Create an object to hold the newly-created event.
     var eventObj = {
       time: eventHour,
       event: eventText,
     };
-    events.push(eventObj);
+
+    // If we already have any events in the array, push the new one into the array.
+    // If not, initialize the array with this new object.
+    if (events != undefined) {
+      events.push(eventObj);
+    } else {
+      events = [
+        {
+          time: eventHour,
+          event: eventText,
+        },
+      ];
+    }
+
+    // Store the events array in localstorage for safekeeping.
     localStorage.setItem("calendar", JSON.stringify(events));
-    console.log(events);
+
+    // Store the event description in a global variable to make sure that it overwrites any previous event at this time.
+    txt = eventText;
   });
 });
 
-$("#clear").click(function () {
-  console.log("emptying localstorage");
+// Make the clear button delete local storage AND the events on the screen.
+$("#clearBtn").click(function () {
   localStorage.clear();
+  $(".event").empty();
 });
