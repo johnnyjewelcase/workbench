b2e021a7c69848bd506db52c1ccfafd439879de1
johnnyjewelcase <61571390+johnnyjewelcase@users.noreply.github.com>
2020-04-04 17:42:48 -0700

    hw5 done

--- Homework/05-Calendar/assets/js/script.js
+++ Homework/05-Calendar/assets/js/script.js
@@ -6,31 +6,7 @@ $(document).ready(function () {
   var currentHour = moment().format("k");
 
   // Create an array of hours in the workday.
-  var hours = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-  ];
+  var hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
   // Create a global array to store objects representing calendar events.
   var events;
